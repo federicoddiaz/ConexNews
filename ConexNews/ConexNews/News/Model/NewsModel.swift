7c649c1d9dc3ddc2504bc8f34c034d522ccbed87
Federico Diaz <federico.dario.diaz@gmail.com>
2024-05-20 19:31:59 -0300

    News model struct implemented

--- ConexNews/ConexNews/News/Model/NewsModel.swift
+++ ConexNews/ConexNews/News/Model/NewsModel.swift
@@ -7,6 +7,36 @@
 
 import Foundation
 
-struct New: Codable {
-    
+struct Post: Codable {
+    var id: Int?
+    var slug: String?
+    var url: String?
+    var title: String?
+    var content: String?
+    var image: String?
+    var thumbnail: String?
+    var status: Status?
+    var category: Category?
+    var publishedAt: String?
+    var updatedAt: String?
+    var userID: Int?
+
+    enum CodingKeys: String, CodingKey {
+        case id, slug, url, title, content, image, thumbnail, status, category, publishedAt, updatedAt
+        case userID = "userId"
+    }
 }
+
+enum Category: String, Codable {
+    case elementum = "elementum"
+    case ipsum = "ipsum"
+    case jsonplaceholder = "jsonplaceholder"
+    case lorem = "lorem"
+    case rutrum = "rutrum"
+}
+
+enum Status: String, Codable {
+    case published = "published"
+}
+
+typealias Posts = [Post]
