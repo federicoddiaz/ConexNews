e1d00498457f796a4b2729fdfb9f604441796558
Federico Diaz <federico.dario.diaz@gmail.com>
2024-05-19 21:39:33 -0300

    <Initial commit>

---
+++ ConexNews/ConexNews/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ConexNews
+//
+//  Created by Federico Díaz on 19/05/2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
